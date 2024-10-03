451624c35a0dccdff30f300b6ae1e37a5a1391f0
alphasin7 <tangaraaymen@outlook.com>
2024-10-03 08:09:54 +0100

    init js

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tp2_ionic_vuejs',
+  webDir: 'dist'
+};
+
+export default config;
